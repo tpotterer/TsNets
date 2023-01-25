9c6ede0158b8d06e41e3f069440039162b338f2c
Tom Potter <tpotter@solomonic.co.uk>
2023-01-25 12:30:41 +0000

    Adding a type export

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -4,6 +4,6 @@ export * from "./layer-base.class";
 export * from "./model-modes.enum";
 export * from "./non-linearity-types.enum";
 export * from "./operation-types.enum";
-
+export * from "./layer-types.enum";
 export * from "./neuron/neuron.class";
 export * from "./atom/atom.class";
